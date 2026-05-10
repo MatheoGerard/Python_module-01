425e1942a52cd80d0fccc19f268b3c738493fcfb
Matheo Gerard <mgerard@fu-r4-p7.s19.be>
2026-05-10 12:35:23 +0200

    add ex3

--- ex3/ft_plant_factory.py
+++ ex3/ft_plant_factory.py
@@ -11,10 +11,22 @@ class Plant:
     def add_age(self):
         self.age += 1
 
-    def show(self):
-        print(f"{self.name}: {round(self.height, 1)}cm, {self.age} days old")
+    def show(self) -> str:
+        return f"{self.name}: {round(self.height, 1)}cm, {self.age} days old"
 
 
-def plant_factory():
+def plant_factory() -> None:
+    plants = [
+            Plant("Rose", 25.0, .8, 30),
+            Plant("Oak", 200.0, .2, 365),
+            Plant("Cactus", 5.0, .1, 90),
+            Plant("Sunflower", 80.0, 1.2, 45),
+            Plant("Fern", 15.0, .7, 120)
+    ]
+    for plant in plants:
+        print("Created: " + plant.show())
+
+
+if __name__ == "__main__":
     print("=== Plant Factory Output ===")
-
+    plant_factory()
